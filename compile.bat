92362d4f3674cafa541c5e36295524cf750b5fb2
superx101 <2467028684@qq.com>
2022-12-24 15:33:05 +0800

    style: modify some  file

--- compile.bat
+++ compile.bat
@@ -2,5 +2,7 @@ xcopy "test\" "nodejs/CALL/test\" /Y
 xcopy "third-party\" "nodejs/CALL/third-party\" /Y
 xcopy "config\" "nodejs/CALL/config\" /Y
 xcopy "src/plugin/ShapeTemplates\" "nodejs/CALL/src/plugin/ShapeTemplates\" /Y
+xcopy "package.json" "nodejs/CALL/package.json" /Y
 tsc
-for /r nodejs\CALL\ %%i in (.\*.js) do uglifyjs %%i -c -m -o %%i
+for /r nodejs\CALL %%i in (.\*.js) do uglifyjs %%i -c -m -o %%i
+@REM for /r nodejs\CALL %i in (.\*.js) do uglifyjs %i -c -m -o %i
