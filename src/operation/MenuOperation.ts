4e838de2c87068439c4e58f072439c59058cf6b1
superx101 <2467028684@qq.com>
2022-12-28 22:34:25 +0800

    Fix: Fix menu subdirectory cmd

--- src/operation/MenuOperation.ts
+++ src/operation/MenuOperation.ts
@@ -2,7 +2,13 @@ import PlayerData from "../model/PlayerData";
 import Menu from "../views/Menu";
 
 export default class MenuOperation {
-    static start(player:Player, output:CommandOutput, playerData:PlayerData, res: { option: number[]; }) {
-        new Menu(player, playerData).sendForm(res.option);
+    static start(player:Player, output:CommandOutput, playerData:PlayerData, res: { option: string; }) {
+        let option: number[];
+        if(res.option != null) 
+            option = res.option.match(/\d+/g).map(Number);
+        else 
+            option = [];
+
+        new Menu(player, playerData).sendForm(option);
     }
 }
