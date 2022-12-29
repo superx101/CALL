deb9f89c1b68b8c33b1b0310ffd7a8889053783b
superx101 <2467028684@qq.com>
2022-12-29 14:01:54 +0800

    Fix: Fix CALL.zip/CALL is empty

--- pack.py
+++ pack.py
@@ -43,9 +43,18 @@ with open('./nodejs/call/package.json', encoding="utf-8") as f:
 
 #CALL-x.x.x.zip
 zip_file = zipfile.ZipFile('./output/CALL-' + version + '.zip', 'w')
-zip_file.writestr('安装说明.txt', '[安装]\n初次安装时需将CALL和CALL.llplugin放入plugins目录\n\n[更新]\n手动更新安装时只需将CALL.llplugin放入plugins目录\n可在配置中开启自动更新')
-zip_file.write('output/temp/CALL', 'CALL', compress_type=zipfile.ZIP_DEFLATED)
+zip_file.writestr('安装说明.txt', '[安装]\n初次安装时需将CALL和CALL.llplugin放入plugins目录\n\n[更新]\n手动更新安装时只需将CALL.llplugin放入plugins目录, 且更新替换CALL/plugin/下的形状包即可\n可在配置中开启自动更新')
+# zip CALL/
+for root, dirs, files in os.walk('output/temp/CALL'):
+    for file in files:
+        file_path = os.path.join(root, file)
+        # 计算文件在压缩包中的路径
+        arc_name = os.path.relpath(file_path, 'output/temp')
+        zip_file.write(file_path, arc_name)
+
+zip_file.writestr(zipfile.ZipInfo('CALL/temp/'), '')#创建空文件夹temp
 zip_file.write('output/temp/CALL.llplugin', "CALL.llplugin", compress_type=zipfile.ZIP_DEFLATED)
+zip_file.close()
 
 shutil.rmtree("./output/temp")
 
