05603c150a9337cda7c34f57ac0e82ebf9adc00e
superx101 <2467028684@qq.com>
2023-01-01 15:23:00 +0800

    Fix: Ignore templates when packing

--- pack.py
+++ pack.py
@@ -9,9 +9,11 @@ with open('compile.py', "r") as f:
     code = f.read()
 exec(code)
 
-for root, dirs, files in os.walk("nodejs/CALL"):
+for root, dirs, files in os.walk("nodejs/call"):
     if "node_modules" in dirs:
         dirs.remove("node_modules")
+    if "templates" in dirs:
+        dirs.remove("templates")
     for file in files:
         if file.endswith(".js"):
             full_path = os.path.join(root, file)
@@ -43,7 +45,7 @@ with open('./nodejs/call/package.json', encoding="utf-8") as f:
 
 #CALL-x.x.x.zip
 zip_file = zipfile.ZipFile('./output/CALL-' + version + '.zip', 'w')
-zip_file.writestr('安装说明.txt', '[安装]\n初次安装时需将CALL和CALL.llplugin放入plugins目录\n\n[更新]\n手动更新安装时只需将CALL.llplugin放入plugins目录, 且更新替换CALL/plugin/下的形状包即可\n可在配置中开启自动更新')
+zip_file.writestr('安装说明.txt', '[安装]\n初次安装时需将CALL和CALL.llplugin放入plugins目录\n\n[更新]\n手动更新安装时只需将CALL.llplugin放入plugins目录, 且更新替换CALL/plugin/下的形状包即可\n\n可在配置中开启自动更新')
 # zip CALL/
 for root, dirs, files in os.walk('output/temp/CALL'):
     for file in files:
