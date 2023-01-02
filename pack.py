699398afdf6c1a910d0b3706a5af4112ac3a3c2f
superx101 <2467028684@qq.com>
2023-01-02 20:05:47 +0800

    Style: Modify some introduction

--- pack.py
+++ pack.py
@@ -45,7 +45,7 @@ with open('./nodejs/call/package.json', encoding="utf-8") as f:
 
 #CALL-x.x.x.zip
 zip_file = zipfile.ZipFile('./output/CALL-' + version + '.zip', 'w')
-zip_file.writestr('安装说明.txt', '[安装]\n初次安装时需将CALL和CALL.llplugin放入plugins目录\n\n[更新]\n手动更新安装时只需将CALL.llplugin放入plugins目录, 且更新替换CALL/plugin/下的形状包即可\n\n可在配置中开启自动更新')
+zip_file.writestr('安装说明.txt', '[安装]\n初次安装时需将CALL和CALL.llplugin放入plugins目录。\n\n[手动更新]\n手动更新安装时只需将CALL.llplugin放入plugins目录, 且更新替换CALL/plugin/下的形状包即可。\n\n[自动更新]\n可在配置中开启自动检查更新, 或在后台输入/call u 检查并自动更新')
 # zip CALL/
 for root, dirs, files in os.walk('output/temp/CALL'):
     for file in files:
